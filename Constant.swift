d930cb342d88a1275eccfc49d1ae157dd90448e1
Beandip <brando.lugo@gmail.com>
2021-06-20 20:56:22 -0500

    Initial start of core data

---
+++ Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote (iOS)
+//
+//  Created by Brando Lugo on 6/20/21.
+//
+
+import SwiftUI
+
+//MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX
